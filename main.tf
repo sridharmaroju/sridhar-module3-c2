95739aeaf77b04d7eeb22c45125d13a5ef5b0394
Sridhar <sridhar.maroju@yahoo.com>
2025-10-22 22:10:23 +0800

    test 11

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "{data.aws_caller_identity.current.arn}")[1] #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
+  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1] #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+  bucket = local.name_prefix-s3-tf-bkt-local.account_id
 }
