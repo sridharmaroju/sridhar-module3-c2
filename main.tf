8150611c2df08ed2e73049da82abdd6631ae58c3
Sridhar <sridhar.maroju@yahoo.com>
2025-10-22 21:32:10 +0800

    test 6

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
-    key    = "sridhar-s3-tf-ci.tfstate"  #Change this
+    key    = "sridhar-s3-tf-ci.tfstate" #Change this
     region = "us-east-1"
   }
 }
@@ -13,10 +13,10 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "${split("/", "${data.aws_caller_identity.current.arn}")[1]}" #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
-  account_id  = "${data.aws_caller_identity.current.account_id}"
+  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1] #if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
+  account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
-  }
+}
